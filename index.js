4440819bac77cdcc4bfc36b4dd24cdbd08358977
mohithingorani <mohithingorani2003@gmail.com>
2025-05-25 19:44:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.json({
+        message:"Hello World!"
+    })
+})
+
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+    console.log("Server running on port ", PORT);
+})
